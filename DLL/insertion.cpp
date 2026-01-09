a55229bf19b18ef38f8cb623661c92046572401a
himanshu bansal <himanshubansalbxn2004@gmail.com>
2026-01-10 02:57:43 +0530

    add file

--- DLL/insertion.cpp
+++ DLL/insertion.cpp
@@ -1,44 +1,48 @@
-#include<iostream>
-using namespace std;    
+#include <iostream>
+using namespace std;
 
-class Node{
-    public:
+class Node
+{
+public:
     int data;
-    Node*next;
-    Node*prev;
-    Node(int value){
+    Node *next;
+    Node *prev;
+    Node(int value)
+    {
         data = value;
         next = NULL;
         prev = NULL;
     }
 };
 
-Node*crateDLL(int arr[] , int index , int size, Node* back){
+Node *crateDLL(int arr[], int index, int size, Node *back)
+{
     // base case
-    if(index == size){
+    if (index == size)
+    {
         return NULL;
     }
-    //node crweate karo
-    Node* temp = new Node(arr[index]);
+    // node crweate karo
+    Node *temp = new Node(arr[index]);
     temp->prev = back;
     temp->next = crateDLL(arr, index + 1, size, temp);
     return temp;
 }
 
-int main(){
-    Node*Head;
+int main()
+{
+    Node *Head;
     Head = NULL;
 
-    Node*tail = NULL;
+    Node *tail = NULL;
 
     // create doubly linked list
-    int arr[] = {10,20,30,40,50};
+    int arr[] = {10, 20, 30, 40, 50};
 
-    // create dll 
+    // create dll
 
     Head = crateDLL(arr, 0, 5, NULL);
 
-    
     // for(int i= 0 ; i< 5; i++){
     //     if(Head == NULL){
     //         Head = new Node(arr[i]);
@@ -52,11 +56,10 @@ int main(){
     //     }
     // }
 
-
     // insert at start
     // if(Head == NULL){
     //     Head = new Node(5); }
-    // else{   
+    // else{
     //     Node* temp = new Node(5);
     //     temp->next = Head;
     //     Head->prev = temp;
@@ -65,7 +68,7 @@ int main(){
 
     // insert at end
     // if(Head == NULL){
-    //     Head = new Node(60); 
+    //     Head = new Node(60);
     // }
     // else{
     //     Node*curr = Head;
@@ -77,8 +80,58 @@ int main(){
     //     temp->prev = curr;
     // }
 
+    // insert at specific position
+    int pos = 3; // 0 based indexing
+
+    // insert at pos ==0
+    if (pos == 0)
+    {
+        // ll exists naa kare
+        if (Head == NULL)
+        {
+            Head = new Node(25);
+        }
+        // ll exists
+        else
+        {
+            Node *temp = new Node(25);
+            temp->next = Head;
+            Head->prev = temp;
+            Head = temp;
+        }
+    }
+    else
+    {
+        // ll at end
+
+        Node *curr = Head;
+        while (--pos)
+        {
+            curr = curr->next;
+        }
+
+        // if insert in ll at end
+        if (curr->next == NULL)
+        {
+            Node *temp = new Node(25);
+            curr->next = temp;
+            temp->prev = curr;
+            return 0;
+        }
+        // if insert in middle
+        else
+        {
+            Node *temp = new Node(25);
+            temp->next = curr->next;
+            temp->prev = curr;
+            curr->next = temp;
+            temp->next->prev = temp;
+        }
+    }
+
     // print the linked list
-    while(Head != NULL){
+    while (Head != NULL)
+    {
         cout << Head->data << " ";
         Head = Head->next;
     }
