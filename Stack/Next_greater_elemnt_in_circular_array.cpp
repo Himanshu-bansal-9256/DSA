cbe3cf8e60f64523e8950349a92d61ff139f0865
himanshu bansal <himanshubansalbxn2004@gmail.com>
2026-01-24 01:34:43 +0530

    add file

--- Stack/Next_greater_elemnt_in_circular_array.cpp
+++ Stack/Next_greater_elemnt_in_circular_array.cpp
@@ -5,17 +5,19 @@ int main()
 {
     // next greater element in circular array
     vector<int> arr = {4, 5, 2, 25};
-    vector<int> ans(arr.size(), -1);
+    vector<int> ans(arr.size(), 1);
     stack<int> st;
-    for(int i = 0  ; i < 2*arr.size() ; i++){
-        int idx = i % arr.size();
-        while(!st.empty() && arr[st.top()] < arr[idx]){
-            ans[st.top()] = arr[idx];
+    for(int i = arr.size()-1  ; i >=0 ; i--){
+        while(!st.empty() && arr[st.top()] < arr[i]){
+            ans[st.top()] = st.top()-i;
             st.pop();
         }
-        st.push(idx); 
+        st.push(i); 
     }
-    
+    while(!st.empty()){
+        ans[st.top()] = st.top()+1;
+        st.pop();
+    }
     for(int i = 0; i < ans.size(); i++){
         cout << ans[i] << " ";
     }
