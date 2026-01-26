10c7f4e33cd47beed331d051a58db86f11b14707
himanshu bansal <himanshubansalbxn2004@gmail.com>
2026-01-26 21:40:42 +0530

    add file

---
+++ Stack/the_celebraty_problem.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int celebrity(vector<vector<int>>& mat) {
+        // code here
+        stack<int>st;
+        int n = mat.size();
+        for(int i = n-1; i>=0;i--){
+            st.push(i);
+        }
+        int first,second;
+        while(st.size()>1){
+            first = st.top();
+            st.pop();
+            second = st.top();
+            st.pop();
+            if(mat[first][second] && !mat[second][first])
+            st.push(second);
+            else if(!mat[first][second] && mat[second][first])  
+            st.push(first);
+        }
+        if(st.empty())
+        return -1;
+        int num = st.top();
+        st.pop();
+        int row = 0 ;
+        int col = 0 ;
+        for(int i = 0 ; i < n; i++){
+            row += mat[num][i]; 
+            col += mat[i][num]; 
+        }
+        return row==0&&col==n-1 ? num:-1;
+    }
+int main()
+{
+    // celebrity problem
+     vector<vector<int>> mat = {
+        {0, 0, 1, 0},
+        {0, 0, 1, 0},
+        {0, 0, 0, 0},
+        {0, 0, 1, 0}
+     };
+    cout << "Celebrity is at index = "
+         << celebrity(mat) << endl;
+
+    return 0;
+}
