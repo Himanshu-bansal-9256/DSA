e662b82dcb6322a12c1f13eb659f82502f937b28
himanshu bansal <himanshubansalbxn2004@gmail.com>
2026-02-16 16:52:20 +0530

    add file

--- Array/moveAllNegativeElementsToTheEnd.cpp
+++ Array/moveAllNegativeElementsToTheEnd.cpp
@@ -10,17 +10,39 @@ int main()
     {
         cin >> arr[i];
     }
+
+    // method 1
+    // for (int i = 0; i < n; i++)
+    // {
+    //     if (arr[i] >= 0)
+    //     {
+    //         swap(arr[i], arr[j]);
+    //         j++;
+    //     }
+    // }
+    // for(int i = 0; i < n; i++)
+    // {
+    //     cout << arr[i] << " ";
+    // }
+
+    // method 2
+    vector<int> temp;
+
     for (int i = 0; i < n; i++)
     {
         if (arr[i] >= 0)
-        {
-            swap(arr[i], arr[j]);
-            j++;
-        }
+            temp.push_back(arr[i]);
     }
-    for(int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] < 0)
+            temp.push_back(arr[i]);
+    }
+    for (int i = 0; i < n; i++) {
+        arr[i] = temp[i];
+    }
+    for (int i = 0; i < n; i++)
     {
         cout << arr[i] << " ";
     }
-    
 }
