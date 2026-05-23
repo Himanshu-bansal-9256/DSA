925253cdd782290f6e75ec0f624ba71a6eb24e7d
himanshu bansal <himanshubansalbxn2004@gmail.com>
2026-05-24 02:48:43 +0530

    new file

--- BinarySearch/Find_Smallest_Letter_Greater_Than_Target.cpp
+++ BinarySearch/Find_Smallest_Letter_Greater_Than_Target.cpp
@@ -2,22 +2,42 @@
 using namespace std;
 class Solution {
 public:
+    // char nextGreatestLetter(vector<char>& arr, char target) {
+    //     // binary search method 
+    //     int low = 0;
+    //     int high = arr.size() - 1;
+
+    //     while(low <= high) {
+
+    //         int mid = low + (high - low) / 2;
+
+    //         if(arr[mid] <= target)
+    //             low = mid + 1;
+    //         else
+    //             high = mid - 1;
+    //     }
+
+    //     return arr[low % arr.size()];
+    // }
+
+    // using vector method
     char nextGreatestLetter(vector<char>& arr, char target) {
-
-        int low = 0;
-        int high = arr.size() - 1;
-
-        while(low <= high) {
-
-            int mid = low + (high - low) / 2;
-
-            if(arr[mid] <= target)
-                low = mid + 1;
-            else
-                high = mid - 1;
+        vector<int>v1;
+        for(int i = 0 ; i < arr.size(); i++){
+            v1.push_back(arr[i]-target);
         }
+        int index = -1;
 
-        return arr[low % arr.size()];
+        for(int i = 0 ; i <v1.size();i++){
+            if(v1[i]>0){
+                index = i;
+                break;
+            }
+        }
+        if(index == -1)
+        return arr[0];
+
+        return arr[index];
     }
 };
 int main() {
